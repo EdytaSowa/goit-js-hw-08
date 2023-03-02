0d02dd3e4137a89a78828fc4b0e7a7fae1e9b560
EdytaSowa <113289486+EdytaSowa@users.noreply.github.com>
2023-03-02 16:29:48 +0100

    zadanie

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector('.gallery');
+
+const newGallery = galleryItems
+  .map(
+    image => `
+    <a class="gallery__item" href="${image.original}">
+      <img
+        class="gallery__image"
+        src="${image.preview}"
+        alt="${image.description}"
+      />
+    </a>`
+  )
+  .join(``);
+list.insertAdjacentHTML('beforeend', newGallery);
+
+
+
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
