f53f196c6db1601c08c8ec587557778d27193269
EdytaSowa <113289486+EdytaSowa@users.noreply.github.com>
2023-03-04 15:14:49 +0100

    update 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,32 +2,29 @@ import { throttle } from 'lodash';
 
 const form = document.querySelector('.feedback-form');
 
-const emailAdress = document.querySelector('input[name="email"]');
-const messageUser = document.querySelector('textarea[name="message"]');
+// const emailAdress = document.querySelector('input');
+// const messageUser = document.querySelector('textarea');
 
 const completeData = throttle(evt => {
   evt.preventDefault();
-
   const user = {
     email: form.elements.email.value,
     message: form.elements.message.value,
   };
-
   localStorage.setItem('feedback-form-state', JSON.stringify(user));
-
 
 }, 500);
 
 const saveDate = localStorage.getItem('feedback-form-state');
 
-console.log(saveDate);
+console.log(`saveDate: ${saveDate}`);
 const parseData = JSON.parse(saveDate);
 
-console.log(parseData);
+// console.log(parseData.email);
+form.elements.email.value = parseData.email;
+form.elements.message.value = parseData.message;
 
 
-emailAdress.value = parseData.email;
-messageUser.value = parseData.message;
 
 function consoleMessage(evt) {
   evt.preventDefault();
