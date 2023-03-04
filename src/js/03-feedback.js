0429faed840326da78de055e0b2c832aeb7a659c
EdytaSowa <113289486+EdytaSowa@users.noreply.github.com>
2023-03-04 16:32:32 +0100

    3 update 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,43 +1,42 @@
 import { throttle } from 'lodash';
 
 const form = document.querySelector('.feedback-form');
-
-// const emailAdress = document.querySelector('input');
-// const messageUser = document.querySelector('textarea');
-
-const completeData = throttle(evt => {
-  evt.preventDefault();
-  const user = {
-    email: form.elements.email.value,
-    message: form.elements.message.value,
-  };
-  localStorage.setItem('feedback-form-state', JSON.stringify(user));
-
-}, 500);
-
-const saveDate = localStorage.getItem('feedback-form-state');
-
-console.log(`saveDate: ${saveDate}`);
-const parseData = JSON.parse(saveDate);
-
-// console.log(parseData.email);
-form.elements.email.value = parseData.email;
-form.elements.message.value = parseData.message;
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
 
 
+form.addEventListener(
+  'input',
+  throttle(e => {
+    e.preventDefault();
+    const user = {
+      email: email.value,
+      message: message.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(user));
+  }, 500)
+);
 
-function consoleMessage(evt) {
-  evt.preventDefault();
+form.addEventListener('submit', e => {
+  e.preventDefault();
   localStorage.removeItem('feedback-form-state');
 
   let user = {
-    email: form.elements.email.value,
-    message: form.elements.message.value,
+    email: email.value,
+    message: message.value,
   };
-
   console.log(user);
   form.reset();
-}
+});
 
-form.addEventListener('input', completeData);
-form.addEventListener('submit', consoleMessage);
+window.addEventListener("load", () => {
+    
+    const saveDate = localStorage.getItem('feedback-form-state');
+    const parseData = JSON.parse(saveDate);
+
+    email.value = parseData.email;
+    message.value = parseData.message;
+
+
+})
+
